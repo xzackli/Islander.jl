178c8c2e6b437fa5618c52b471ea926383bfc32a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-30 21:31:25 +0000

    build based on 5ad7890

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Islander.jl-1","page":"Home","title":"Islander.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Islander will be a nice component separation code someday!","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Islander, Pixell]","category":"page"}]
+[{"location":"#Islander.jl-1","page":"Home","title":"Islander.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Islander will be a nice component separation code someday!","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Islander]\nPages = [\"Islander.jl\", \"Pixell.jl\"]","category":"page"}]
 }
