351374fd21510711d9d492001e6cb8b77a30711b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-30 20:06:21 +0000

    build based on 6cfbc8d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Islander.jl-1","page":"Home","title":"Islander.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Islander]","category":"page"}]
+}
